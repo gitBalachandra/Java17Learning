98e66247d60cd41cdf0c843ae9ce25ad38a7f55b
balachandra <balachandra.kun@gmail.com>
2026-09-06 20:03:01 +0530

    july 20 branch constructor class

--- src/com/learning/day5/ArrayExample.java
+++ src/com/learning/day5/ArrayExample.java
@@ -1,4 +1,10 @@
 package com.learning.day5;
 
 public class ArrayExample {
+    public static void main(String[] args){
+        int[] num = {1, 2, 3, 4, 5};
+        for(int n : num){
+            System.out.print(n + " ");
+        }
+    }
 }
